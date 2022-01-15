8a359f7acf05421aa7671c7f12d198f959489a59
Nathanael White <narhanaelw99@gmail.com>
2022-01-14 21:21:18 -0600

    initial commit and game set up

---
+++ src/ReactRogue.js
@@ -0,0 +1,23 @@
+import React, { useRef, useEffect } from "react";
+
+const ReactRogue = ({ width, height, tilesize }) => {
+  const canvasRef = React.useRef();
+  useEffect(() => {
+    console.log("draw to canvas");
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.clearRect(0,0,width * tilesize, height * tilesize)
+    ctx.fillStyle='#000';
+    ctx.fillRect(12, 22, 16, 16);
+  });
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={width * tilesize}
+      height={height * tilesize}
+      style={{ border: "1px solid black" }}
+    ></canvas>
+  );
+};
+
+export default ReactRogue;
