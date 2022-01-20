bb81feb97b1395a8c12cec4e541873eaef4f910e
Nathanael White <narhanaelw99@gmail.com>
2022-01-19 18:57:54 -0600

    created a game world component and connected walls

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -1,16 +1,18 @@
 import React, { useRef, useEffect, useState } from "react";
 import InputManager from "./InputManager.js";
 import Player from "./Player.js";
+import World from "./World";
 
 const ReactRogue = ({ width, height, tilesize }) => {
   const canvasRef = React.useRef();
   const [player, setPlayer] = useState(new Player(1, 2, tilesize));
+  const [world, setWorld] = useState(new World(width, height, tilesize));
   let inputManager = new InputManager();
   const handleInput = (action, data) => {
     console.log(`handle input: ${action}: ${JSON.stringify(data)}`);
     let newPlayer = new Player();
-    Object.assign(newPlayer,player);
-    newPlayer.move(data.x,data.y);
+    Object.assign(newPlayer, player);
+    newPlayer.move(data.x, data.y);
     setPlayer(newPlayer);
   };
   useEffect(() => {
@@ -27,6 +29,7 @@ const ReactRogue = ({ width, height, tilesize }) => {
     console.log("draw to canvas");
     const ctx = canvasRef.current.getContext("2d");
     ctx.clearRect(0, 0, width * tilesize, height * tilesize);
+    world.draw(ctx);
     player.draw(ctx);
   });
 
